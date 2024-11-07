bc7e96ada9d0f4f4a72317273025f7faf78516a9
Mael Donnart <maeldonnart@hotmail.fr>
2024-11-07 09:04:04 +0100

    - SEO: metadata added using the recommended method for next 14 and removal of the CustomHead component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,17 +1,13 @@
 import ContactForm from '@/components/section/contact-form'
 import Footer from '@/components/shared/footer'
-import CustomHead from '@/components/shared/Head'
 import Header from '@/components/shared/header'
 
 export default function Characters() {
   return (
-    <>
-      <CustomHead />
-      <div className="flex flex-col min-h-screen bg-gray-900 text-gray-100">
-        <Header />
-        <ContactForm />
-        <Footer />
-      </div>
-    </>
+    <div className="flex flex-col min-h-screen bg-gray-900 text-gray-100">
+      <Header />
+      <ContactForm />
+      <Footer />
+    </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,51 @@ const geistMono = localFont({
 })
 
 export const metadata: Metadata = {
-  title: 'Shadows of Fortune',
+  title: 'Shadows of Fortune - Embark on Your Blockchain Adventure',
   description:
-    'A hardcore, competitive web3 survival horror / RPG powered by theSonic Blockchain.',
+    'Discover Shadows of Fortune, an immersive and adventurous blockchain-based game experience. Mint assets, collect loot, and explore the world of web3 gaming.',
+  keywords: [
+    'Shadows of Fortune',
+    'blockchain',
+    'game',
+    'mint',
+    'loot',
+    'explore',
+    'web3',
+    'sonic',
+    'ftm',
+    'SoF',
+    'blockchain gaming',
+    'NFT',
+    'crypto game',
+  ],
+  openGraph: {
+    title: 'Shadows of Fortune',
+    description:
+      'Join the adventure in Shadows of Fortune, a blockchain-based gaming experience. Mint exclusive assets, collect valuable loot, and explore the Shadows Fall city.',
+    url: 'https://shadowsoffortune.com',
+    siteName: 'Shadows of Fortune',
+    images: [
+      {
+        url: '/images/splash-and-title.webp',
+        width: 1200,
+        height: 630,
+        alt: 'Shadows of Fortune logo and splash screen',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Shadows of Fortune',
+    description:
+      'Join the adventure in Shadows of Fortune, a blockchain-based gaming experience. Mint exclusive assets, collect valuable loot, and explore the Shadows Fall city.',
+    siteId: '@ShadowsOFortune',
+    creator: '@ShadowsOFortune',
+    images: ['/images/splash-and-title.webp'],
+  },
+  viewport: 'width=device-width, initial-scale=1.0',
   icons: {
     icon: '/favicon.ico',
   },
